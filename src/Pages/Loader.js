0a368df0912ab2627319440e9f71eb9a8225c09a
princedonga <160412994+princedonga@users.noreply.github.com>
2024-10-03 17:48:55 +0530

    upload

--- src/Pages/Loader.js
+++ src/Pages/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className="full-page-loader">
-            <div className="loader"></div>
+            <img src='https://lh6.googleusercontent.com/proxy/VU6tXRjDD6NEUtL3H-bD8w5-h5y1DeAN0ebMIHtg-nR2EzoozhU0A1cnLQtaTtFYg01oIZlvQmpKpOzr' alt=''></img>
         </div>
     )
 }
