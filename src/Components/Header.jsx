69ce32648bf3ff94cdbda9a50d5e78c4b1528379
khushali <amreliyakhushali@gmail.com>
2025-02-21 17:03:10 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3061,7 +3061,7 @@ const Header = () => {
     var upBannerSlider = {
         dots: false,
         infinite: true,
-        speed: 1000,
+        speed: 1500,
         autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 1,
